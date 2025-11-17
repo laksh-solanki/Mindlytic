89a8660fe339b776044d60942ba84cba64b9525d
Laksh Solanki <lakshsolanki848@gmail.com>
2025-11-17 21:12:09 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    main.css -> backend/package.json
    	new file:   backend/server.js
    	renamed:    .editorconfig -> frontend/.editorconfig
    	renamed:    .gitattributes -> frontend/.gitattributes
    	renamed:    .gitignore -> frontend/.gitignore
    	renamed:    .prettierrc.json -> frontend/.prettierrc.json
    	renamed:    .vscode/extensions.json -> frontend/.vscode/extensions.json
    	renamed:    README.md -> frontend/README.md
    	renamed:    index.html -> frontend/index.html
    	renamed:    jsconfig.json -> frontend/jsconfig.json
    	renamed:    package-lock.json -> frontend/package-lock.json
    	renamed:    package.json -> frontend/package.json
    	renamed:    src/App.vue -> frontend/src/App.vue
    	renamed:    src/assets/Picture/CoursePathway_BG.jpg -> frontend/src/assets/Picture/CoursePathway_BG.jpg
    	renamed:    src/assets/Picture/IMCA.jpeg -> frontend/src/assets/Picture/IMCA.jpeg
    	renamed:    src/assets/Picture/L&T EduTech.png -> frontend/src/assets/Picture/L&T EduTech.png
    	renamed:    src/assets/Picture/LnT.png -> frontend/src/assets/Picture/LnT.png
    	renamed:    src/assets/Picture/febinsign.png -> frontend/src/assets/Picture/febinsign.png
    	renamed:    src/assets/Picture/mindlytic-Main-logo.png -> frontend/src/assets/Picture/mindlytic-Main-logo.png
    	renamed:    src/assets/Picture/mindlytic-high-resolution-logo.png -> frontend/src/assets/Picture/mindlytic-high-resolution-logo.png
    	renamed:    src/assets/Picture/mindlytic-logo.png -> frontend/src/assets/Picture/mindlytic-logo.png
    	renamed:    src/assets/Picture/profSignature.png -> frontend/src/assets/Picture/profSignature.png
    	renamed:    src/assets/Picture/profile-pic.webp -> frontend/src/assets/Picture/profile-pic.webp
    	renamed:    src/assets/Picture/sk.jfif -> frontend/src/assets/Picture/sk.jfif
    	renamed:    src/assets/base.css -> frontend/src/assets/base.css
    	renamed:    src/assets/main.css -> frontend/src/assets/main.css
    	renamed:    src/components/Folder.vue -> frontend/src/components/Folder.vue
    	renamed:    src/components/Footer.vue -> frontend/src/components/Footer.vue
    	renamed:    src/components/Navbar.vue -> frontend/src/components/Navbar.vue
    	renamed:    src/main.js -> frontend/src/main.js
    	renamed:    src/router/index.js -> frontend/src/router/index.js
    	renamed:    src/stores/loading.js -> frontend/src/stores/loading.js
    	renamed:    src/views/AboutView.vue -> frontend/src/views/AboutView.vue
    	renamed:    src/views/CertificateView.vue -> frontend/src/views/CertificateView.vue
    	renamed:    src/views/GlobalLoader.vue -> frontend/src/views/GlobalLoader.vue
    	renamed:    src/views/HelpView.vue -> frontend/src/views/HelpView.vue
    	renamed:    src/views/HomeView.vue -> frontend/src/views/HomeView.vue
    	renamed:    src/views/MainSettings.vue -> frontend/src/views/MainSettings.vue
    	renamed:    src/views/NotmatchView.vue -> frontend/src/views/NotmatchView.vue
    	renamed:    src/views/Profile.vue -> frontend/src/views/Profile.vue
    	renamed:    src/views/ProjectHome.vue -> frontend/src/views/ProjectHome.vue
    	renamed:    src/views/ProjectView.vue -> frontend/src/views/ProjectView.vue
    	renamed:    src/views/SettingsView.vue -> frontend/src/views/SettingsView.vue
    	renamed:    vercel.json -> frontend/vercel.json
    	renamed:    vite.config.js -> frontend/vite.config.js

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend working on Render!");
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on ${port}`));
